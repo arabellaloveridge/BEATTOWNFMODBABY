b438164f0e941f7cc19fcdc546a8b2f7ca3eef5e
Sburdon <spencerburdon6@gmail.com>
2024-11-06 19:12:36 -0500

    All Bug Fix

--- BeatTown Milestone 2/Assets/CurrentScripts/Push.cs
+++ BeatTown Milestone 2/Assets/CurrentScripts/Push.cs
@@ -199,7 +199,7 @@ public class Push : MonoBehaviour
         target.position = endPosition;
         Debug.Log($"{target.name} has been pushed to {targetTilePosition}");
 
-        // Update position in OccupiedTilesManager and component-specific CurrentTilePosition
+        // Update position in OccupiedTilesManager for AIMove or BarraMove
         AIMove targetMove = target.GetComponent<AIMove>();
         if (targetMove != null)
         {
@@ -224,6 +224,7 @@ public class Push : MonoBehaviour
             hook.HandleSwingOrPushIntoHook(target.gameObject);
         }
     }
+
 
 
 
